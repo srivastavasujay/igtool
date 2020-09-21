ccb90d5a253659b08d92c838801e25d4e80f05ec
srivastavasujay <sujaysrivastava98@gmail.com>
2020-09-22 02:22:10 +0530

    Script created. nslookup added

---
+++ igtool.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This script automates the task of information gathering for a web server
+
+# Make sure the script is being executed with superuser privileges
+if [[ "${UID}" -ne 0 ]]
+then
+   echo 'Please run with sudo or as root.' >&2
+   exit 1
+fi
+
+# If the user doesn't supply at least one argument, then give them help
+if [[ "${#}" -lt 1 ]]
+then
+  echo "Usage: ${0} WEB_SERVER" >&2
+  echo 'Please use the above mentioned format with the name of the WEB_SERVER you wish to gather information about' >&2
+  exit 1
+fi
+
+# The firsr parameter is the webserver
+WEB_SERVER="${1}"
+
+echo -e "IGTOOL : Automates the task of information gathering\n">ig.txt
+
+# nslookup
+echo -e "1. -----nslookup-----\n">>ig.txt
+nslookup ${WEB_SERVER}>>ig.txt
